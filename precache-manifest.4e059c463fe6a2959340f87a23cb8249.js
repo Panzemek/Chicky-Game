c352957f4d0942caaa4f6e93aba484622460789a
Matthew Zemek <panzemek@gmail.com>
2019-04-26 12:04:49 -0700

    Updates

---
+++ precache-manifest.4e059c463fe6a2959340f87a23cb8249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a22833ef52361ab2964c992aa97715",
+    "url": "/Chicky-Game/index.html"
+  },
+  {
+    "revision": "165e88239463fc1f7b81",
+    "url": "/Chicky-Game/static/css/main.4d164fde.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Chicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "165e88239463fc1f7b81",
+    "url": "/Chicky-Game/static/js/main.76aeaf4c.chunk.js"
+  },
+  {
+    "revision": "303cbf10cc2723a880de",
+    "url": "/Chicky-Game/static/js/runtime~main.540201d8.js"
+  }
+]);
